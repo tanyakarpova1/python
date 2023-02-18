0394199e6849a12b5dbc8b3226977c4e6129ee91
tanyakarpova1 <113306185+tanyakarpova1@users.noreply.github.com>
2023-02-18 13:57:05 +0300

    Update 11.02.py

--- 11.02.py
+++ 11.02.py
@@ -1,13 +1,13 @@
 from random import randint
-a = 250
 b = []
+a = 250
 for c in range(a):
     b.append(randint(0,10000))
 print("Изначальный массив")
 print(b)
 for c in range(a-1):
     for d in range(a-c-1):
-        if b[d] > b[d+1]:
+        if b[d+1] < b[d]:
             b[d], b[d+1] = b[d+1], b[d]
 print("Отсортированный массив")
 print(b)
