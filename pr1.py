868387097d335c806af21ee56b6fe9feff10e477
tanyakarpova1 <113306185+tanyakarpova1@users.noreply.github.com>
2023-05-25 12:31:37 +0300

    Update pr1.py

--- pr1.py
+++ pr1.py
@@ -1,19 +1,3 @@
-print("Задание 2")
-import os
-def longest_words(file):
-    with open(file, "r") as text:
-        words = text.read().split()
-    max_length = len(max(words, key=len))
-    sought_words = [word for word in words if len(word) == max_length]
-    f = "result.txt"
-    i = 1
-    while os.path.exists(f):
-        f = f"result{i}.txt"
-        i += 1
-    with open(f, 'w') as file:
-        file.write(''.join(sought_words) + "\n")
-longest_words('article.txt')
-
 print("Задание 3")
 import csv
 import datetime
