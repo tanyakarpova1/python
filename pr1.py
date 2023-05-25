33ea2bc5bde6d6946ad4ebbb78bc96cad1669016
tanyakarpova1 <113306185+tanyakarpova1@users.noreply.github.com>
2023-05-25 12:34:53 +0300

    pr1.py

--- pr1.py
+++ pr1.py
@@ -8,3 +8,23 @@ with open('rows_300.csv', 'w', newline = '') as f:
     for line in range(1, 301):
         writer.writerow([line, datetime.datetime.now().microsecond])
         time.sleep(0.01)
+        
+print("Задание 4")
+c = [1,5,7]
+d = [3,2,4]
+a = []
+a.insert(0,c[0])
+a.insert(1,d[0])
+a.insert(2,c[1])
+a.insert(3,d[1])
+a.append(c[2])
+a.append(d[2])
+print(a)
+
+print("Задание 5")
+def calculation():
+    q = int(input())
+    w = int(input())
+    print(q+w)
+    print(q-w)
+print(calculation())
